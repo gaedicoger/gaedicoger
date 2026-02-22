48662983caa9f0fbfb7c20e00465009e8302f55d
Gaed <oger.gaedic@gmail.com>
2026-02-22 13:48:57 +0100

    Début etapes 5 lier les boutons

--- Exercices/guessmynumber/script.js
+++ Exercices/guessmynumber/script.js
@@ -1,5 +1,41 @@
-//J'appelle une fenêtre pour demander à l'utilisateur de choisir un nombre. La valeur est stockée dans une variable.
+// Je sors les nombres de la fonction gameplay pour les stocker :
+let numberToGuess;
+let givenNumber;
 
+//Je récupère le bouton 1, j'écoute le click et je récupère le nombre:
+document
+  .querySelector("#buttonPlayerOne")
+  .addEventListener("click", function () {
+    numberToGuess = Number(document.querySelector("#inputPlayerOne").value);
+    console.log(numberToGuess);
+  });
+//Je récupère le bouton 2, j'écoute le click et je récupère le nombre
+document
+  .querySelector("#buttonPlayerTwo")
+  .addEventListener("click", function () {
+    givenNumber = Number(document.querySelector("#inputPlayerTwo").value);
+    console.log(givenNumber);
+  });
+
+//J'appelle une fenêtre pour demander à un joueur de choisir un nombre entre 0 et 50.
+function askPlayerOne() {
+  let givenNumber2 = Number(
+    prompt(
+      "Joueur 1 : Veuillez proposer un nombre entre 0 et 50",
+      "Votre nombre",
+    ),
+  );
+
+  // Je vérifie que ce nombre et bien compris entre 0 et 50.
+  while (givenNumber2 < 0 || givenNumber2 > 50) {
+    givenNumber2 = Number(
+      prompt("Attention, un nombre entre 0 et 50 !", "Votre nombre"),
+    );
+  }
+  return givenNumber2;
+}
+
+//J'appelle une fenêtre pour demander au joueur 2 d'essayer de deviner le nombre du joueur 1:
 function askNumber() {
   let givenNumber = Number(
     prompt(
@@ -11,7 +47,7 @@ function askNumber() {
 }
 //console.log(askNumber());
 
-//Je crée une fonction qui va comparer le nombre donné par l'utilisateur au nombre 22.
+//Je crée une fonction qui va comparer le nombre donné par le joueur 1 au nombre donné par le joueur 2.
 function didWin(givenNumber, numberToGuess) {
   if (givenNumber === numberToGuess) {
     return true;
@@ -20,14 +56,16 @@ function didWin(givenNumber, numberToGuess) {
   }
 }
 
-function gamePlay() {
-  let numberToGuess = askPlayerOne(); //récupère le chiffre 1
-  let givenNumber = askNumber(); //récupérer le chiffre 2
+//La fonction gamePlay qui orchestre toutes les fonctions :
+/*function gamePlay() {
+  numberToGuess = askPlayerOne(); //récupère le chiffre 1
+  givenNumber = askNumber(); //récupérer le chiffre 2
   //console.log(givenNumber, typeof givenNumber);
   let hasWon = didWin(givenNumber, numberToGuess); //stocker le nombre et le vérifier
   console.log(hasWon);
 
   while (!hasWon) {
+    //vérifie et donne des indices pour aider le joueur 2
     if (givenNumber > numberToGuess) {
       alert("Plus petit !");
     } else if (givenNumber < numberToGuess) {
@@ -40,22 +78,4 @@ function gamePlay() {
   alert("Bravo ! Le nombre à deviner était bien : " + numberToGuess + " !");
 }
 
-gamePlay();
-
-//Etape 4 en cours :
-
-function askPlayerOne() {
-  let givenNumber2 = Number(
-    prompt(
-      "Joueur 1 : Veuillez proposer un nombre entre 0 et 50",
-      "Votre nombre",
-    ),
-  );
-
-  while (givenNumber2 < 0 || givenNumber2 > 50) {
-    givenNumber2 = Number(
-      prompt("Attention, un nombre entre 0 et 50 !", "Votre nombre"),
-    );
-  }
-  return givenNumber2;
-}
+gamePlay();*/
