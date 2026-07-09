7693409085df2e8029a0aa679c1bd7d84bf2145b
Gaed <oger.gaedic@gmail.com>
2026-07-09 17:11:28 +0200

    clean: suppression définitive des anciens dossiers

--- Exercices_live/lundi9fevJS/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Booooccchouuurrran");
-
-//Exercice 1 :
-let firstname = "Gaédic";
-const age = 38;
-console.log(firstname, age);
-console.log("Je m'appelle " + firstname + " et j'ai " + age + " ans.");
